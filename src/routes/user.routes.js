716ff02a4a84d856cb466939e10ad6748cbff1cb
rishabh200012 <rishabhsharma200012@gmail.com>
2026-06-23 22:03:53 +0530

    change in user route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,13 +10,13 @@ import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const userRoute = Router();
 
-userRoute.post("/user/register", userRegister);
-userRoute.post("/user/login", userLogin);
-userRoute.post("/user/verify-otp", otpVerification);
-userRoute.get("/user/auth-check", authMiddleware, (req, res) =>
+userRoute.post("/register", userRegister);
+userRoute.post("/login", userLogin);
+userRoute.post("/verify-otp", otpVerification);
+userRoute.get("/auth-check", authMiddleware, (req, res) =>
   res.status(200).json({ success: true, message: "User Authorised" }),
 );
-userRoute.get("/user/chat-users", authMiddleware, getAllUsers);
-userRoute.post("/user/logout", authMiddleware, logoutUser);
+userRoute.get("/chat-users", authMiddleware, getAllUsers);
+userRoute.post("/logout", authMiddleware, logoutUser);
 
 export default userRoute;
